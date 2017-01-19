cece5b65ddc9b14c6c4d72dc9467ef08493c8486
Christina Cook <cfcook@ucsd.edu>
2017-01-19 10:05:46 -0800

    edit profile theme

---
+++ static/js/introHCI.js
@@ -0,0 +1,15 @@
+document.getElementById("demo").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+}
+
+// function projectClick(e) { 
+//     // prevent the page from reloading 
+//     e.preventDefault();
+//     // In an event handler, $(this) refers to 
+//     // the object that triggered the event 
+//     $(this).css("background-color", "#7fff00");
+//
+// }
+//
